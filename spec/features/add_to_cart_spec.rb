86e0468926c819bedc72475c75227ef3044c1ee6
Dawood <dreamboy.dani@gmail.com>
2020-10-20 23:24:02 +0000

    update the add to cart spec

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.feature "User can add item to the cart", type: :feature, js: true do
+
+  before :each do
+    @category = Category.create! name: "Apparel"
+
+    10.times do |n|
+      @category.products.create!(
+        name: Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+  end
+
+  scenario "Add product to the cart" do
+    visit root_path
+
+    expect(page).to have_button("Add", count: 10)
+    
+    first(:button, "Add").click
+    save_screenshot
+
+    expect(page).to have_content("My Cart (1)")
+
+  end
+end
